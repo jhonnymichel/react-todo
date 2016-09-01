68d7868fa8e1b4768e71e0a68fc70b8836c74349
Jhonny Michel <jhonnymichel@outlook.com>
2016-09-01 00:11:11 -0300

    added dismiss animation to toDos

--- app/js/components/Layout.js
+++ app/js/components/Layout.js
@@ -11,24 +11,27 @@ export default class Layout extends React.Component {
       description: "Add a todo and try to accomplish 'em!'",
       todoList: []
     }
+    this.hashIdCounter = 0;
   }
   receiveNewTodo(todo) {
     let todoList = this.state.todoList.slice();
-    todoList.push(todo);
+    todoList.push({
+      text: todo,
+      id: "todo"+this.hashIdCounter++
+    });
     this.setState({todoList});
   }
   deleteToDo(key) {
-    let todoList = this.state.todoList.slice();
-    todoList.splice(key, 1);
+    let todoList = this.state.todoList.filter((todo) => todo.id != key);
     this.setState({todoList});
   }
   render() {
     const title = this.state.title;
     const description = this.state.description;
     let TodoList = this.state.todoList.map((todo, i) =>
-      <ToDo key={i}
-      objId={i}
-      value={todo}
+      <ToDo key={todo.id}
+      objId={todo.id}
+      value={todo.text}
       deleteToDo={this.deleteToDo.bind(this)}/>
     ).reverse();
     return (
--- app/js/components/ToDo.js
+++ app/js/components/ToDo.js
@@ -1,8 +1,35 @@
 import React from "react";
 
-const ToDo = (props) => 
-    <div onClick={() => props.deleteToDo(props.objId)}>
-      <p>{props.value}</p>
-    </div>
+export default class ToDo extends React.Component {
 
-export default ToDo;
+  constructor(props) {
+    super(props);
+    this.state = {
+      objStyle: {
+        transition: "opacity 0.4s ease-out, transform 0.4s ease-out",
+      }
+    };
+  }
+  fadeOut(onEndCallBack) {
+    let objStyle = {...this.state.objStyle};
+    objStyle.opacity = "0";
+    objStyle.transform = "translateX(50%)";
+    this.setState({
+      objStyle
+    });
+    setTimeout(this.setAsDone.bind(this), 400);
+  }
+  setAsDone() {
+    this.props.deleteToDo(this.props.objId);
+  }
+  render() {
+    const objStyle = this.state.objStyle;
+    const clickCallback = this.fadeOut.bind(this);
+    const todoMesage = this.props.value;
+    return (
+      <div style={objStyle} onClick={clickCallback}>
+        <p>{todoMessage}</p>
+      </div>
+    );
+  }
+}
