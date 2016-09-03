8460f0bc978049960263355fc8ca90000be3b993
Jhonny Michel <jhonnymichel@outlook.com>
2016-09-03 01:32:39 -0300

    separated Layout component from ToDoList component

--- app/js/components/Layout.js
+++ app/js/components/Layout.js
@@ -1,44 +1,29 @@
 import React from "react";
 import Header from "./Header";
 import AddBar from "./AddBar";
-import ToDo from "./ToDo";
+import ToDoList from "./ToDoList";
 
 export default class Layout extends React.Component {
   constructor() {
     super();
+    this.idIncrementer = 0;
     this.state = {
       title: "Xexeu's todo",
       description: "Add a todo and try to accomplish 'em!'",
-      todoList: []
     }
-    this.hashIdCounter = 0;
   }
   receiveNewTodo(todo) {
-    let todoList = this.state.todoList.slice();
-    todoList.push({
-      text: todo,
-      id: "todo"+this.hashIdCounter++
-    });
-    this.setState({todoList});
-  }
-  deleteToDo(key) {
-    let todoList = this.state.todoList.filter((todo) => todo.id != key);
-    this.setState({todoList});
+    this.refs['todoList'].receiveNewTodo(todo);
   }
   render() {
-    const title = this.state.title;
-    const description = this.state.description;
-    let TodoList = this.state.todoList.map((todo, i) =>
-      <ToDo key={todo.id}
-      objId={todo.id}
-      value={todo.text}
-      deleteToDo={this.deleteToDo.bind(this)}/>
-    ).reverse();
+    const {title, description} = this.state;
+    const callBack = this.receiveNewTodo.bind(this);
+
     return (
       <div>
         <Header title={title} description={description}/>
-        <AddBar callBack={this.receiveNewTodo.bind(this)}/>
-        {TodoList}
+        <AddBar callBack={callBack}/>
+        <ToDoList ref="todoList" />
       </div>
     );
   }
---
+++ app/js/components/ToDoList.js
@@ -0,0 +1,38 @@
+import React from "react";
+import ToDo from "./ToDo";
+
+export default class ToDoList extends React.Component {
+  constructor() {
+    super();
+    this.idIncrementer = 0;
+    this.state = {
+      todoList: []
+    }
+  }
+  receiveNewTodo(todo) {
+    let todoList = [...this.state.todoList];
+    todoList.push({
+      text: todo,
+      id: "todo"+this.idIncrementer++
+    });
+    this.setState({todoList});
+  }
+  deleteToDo(key) {
+    let todoList = this.state.todoList.filter((todo) => todo.id != key);
+    this.setState({todoList});
+  }
+  render() {
+    let TodoList = this.state.todoList.map((todo, i) =>
+      <ToDo key={todo.id}
+      objId={todo.id}
+      value={todo.text}
+      deleteToDo={this.deleteToDo.bind(this)}/>
+    ).reverse();
+
+    return (
+      <div>
+        {TodoList}
+      </div>
+    );
+  }
+}
