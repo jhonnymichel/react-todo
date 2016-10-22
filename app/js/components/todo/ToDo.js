7bea274651d533271d96109d0208bd466e1a8255
Jhonny Michel <jhonnymichel@outlook.com>
2016-10-22 00:31:57 -0300

    uses toLocaleString to format date infos

--- app/js/components/todo/ToDo.js
+++ app/js/components/todo/ToDo.js
@@ -25,7 +25,7 @@ export default class ToDo extends React.Component {
     state.styles = this.expandAnimator.expand();
     state.details = [
       {
-        title: "Created: ", 
+        title: "Created: ",
         value: this.creationTimeStamp
       },
       {
@@ -51,16 +51,8 @@ export default class ToDo extends React.Component {
 
   getTimeStamp(time) {
     const date = new Date(time);
-    const hours = ("0" + date.getHours()).slice(-2);
-    const minutes = ("0" + date.getMinutes()).slice(-2);
-    const day = ("0" + date.getDate()).slice(-2);
-    const month = ("0" + (date.getMonth() + 1)).slice(-2);
-    const year = date.getFullYear();
-
-    return `${day}/${month}/${year}, ${hours}:${minutes}`;
+    return date.toLocaleString();
   }
-
-  format
 
   updateStatus(e) {
     e.stopPropagation();
