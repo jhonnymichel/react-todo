7f672b6958275d6f0f397e7a35b9a51c5c41149e
Jhonny Michel <jhonnymichel@outlook.com>
2016-10-30 18:50:53 -0300

    changes todo toggle icon

--- app/js/components/todo/DoneToggle.js
+++ app/js/components/todo/DoneToggle.js
@@ -2,7 +2,7 @@ import React from "react";
 
 export default function DoneToggle ({ status, clickHandler }) {
   let css = `material-icons ${status ? 'checked' : 'unchecked'}`;
-  let icon = `${status ? 'check_circle' : 'radio_button_unchecked'}`;
+  let icon = `${status ? 'check_box' : 'check_box_outline_blank'}`;
   return (
     <button
       className="todo__toggle-button"
